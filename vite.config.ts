5a10bf1b99637ce203089d2b07832d8575946692
kararalaa25 <kararalkhafaji20@gmail.com>
2026-05-10 22:24:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/react-swc";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/smileos/",
+export default defineConfig({
+  base: "/fatima-amer/", // Add this line!
   server: {
-    host: "::",
-    port: 8080,
+    host: "0.0.0.0",
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
